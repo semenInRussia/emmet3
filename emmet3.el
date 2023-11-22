4577f41abb432ba16e4730156acc03beaa15b70d
semenInRussia <hrams205@gmail.com>
2023-11-22 22:17:35 +0300

    show in completions Emmet markup instead of preview
    show preview as annotations now

--- emmet3.el
+++ emmet3.el
@@ -28,25 +28,39 @@
 
 (require 'emmet-mode)
 
+
 (defun emmet3-capf ()
   "The completion at point function for Emmet expansion.
 
 Show the result in auto-completion popup."
   (interactive)
-  (save-excursion
-    (if (nth 3 (syntax-ppss))  ; inside string
-        nil
-      (let ((end (point))
-            (beg (emmet-find-left-bound))
-            (collection (list (emmet3--at-point))))
-        (and
-         collection
-         (list beg end
-               collection
-               :company-kind (lambda (_) 'string)
-               ;; :exit-function (lambda (_ st) (and (eq st 'finished)
-               ;;                                    (emmet-expand-line 0)))
-               ))))))
+  (if (nth 3 (syntax-ppss))           ; inside string
+      nil
+    (let* ((end (point))
+           (beg (emmet-find-left-bound))
+           (input (buffer-substring-no-properties beg end))
+           (collection (list input)))
+      (and
+       collection
+       (list beg end
+             collection
+             :company-kind (lambda (_) 'string)
+             :annotation-function (lambda (_) (emmet3--at-point))
+             :exit-function (lambda (_ st) (and (eq st 'finished)
+                                                (emmet-expand-line nil))))))))
+
+(defun emmet3--nosort-table (table)
+  "Create a completion TABLE which won't be sorted with other post commands.
+
+See \"(elisp)Programmed Completion\""
+  (lambda (str pred action)
+    (cond
+     ((eq action 'metadata)
+      '(metadata
+        (display-sort-function . identity)
+        (cycle-sort-function . identity)))
+     (t
+      (complete-with-action action table str pred)))))
 
 (defun emmet3--at-point ()
   "Return a string which is an expansion of an Emmet markup at the cursor."
